f0316cbe1b70e8c5146d576308dc4389cdfe008f
krantheman <akash.akasht.tom@gmail.com>
2024-12-16 13:17:00 +0530

    perf: improve shift assignment schedule migration patch

--- hrms/patches/v15_0/migrate_shift_assignment_schedule_to_shift_schedule.py
+++ hrms/patches/v15_0/migrate_shift_assignment_schedule_to_shift_schedule.py
@@ -1,5 +1,4 @@
 import frappe
-from frappe.utils import unique
 
 from hrms.hr.doctype.shift_schedule.shift_schedule import get_or_insert_shift_schedule
 
@@ -7,11 +6,11 @@ from hrms.hr.doctype.shift_schedule.shift_schedule import get_or_insert_shift_sc
 def execute():
 	frappe.reload_doc("HR", "doctype", "Shift Assignment")
 
-	fields = ["shift_type", "frequency", "employee", "shift_status", "enabled", "create_shifts_after"]
-
-	for doc_name in frappe.get_all("Shift Assignment Schedule", pluck="name"):
-		doc = frappe.db.get_value("Shift Assignment Schedule", doc_name, fields, as_dict=True)
-		repeat_on_days = unique(frappe.get_all("Assignment Rule Day", {"parent": doc_name}, pluck="day"))
+	fields = ["name", "shift_type", "frequency", "employee", "shift_status", "enabled", "create_shifts_after"]
+	for doc in frappe.get_all("Shift Assignment Schedule", fields=fields, as_dict=True):
+		repeat_on_days = frappe.get_all(
+			"Assignment Rule Day", {"parent": doc.name}, pluck="day", distinct=True
+		)
 		shift_schedule_name = get_or_insert_shift_schedule(doc.shift_type, doc.frequency, repeat_on_days)
 
 		schedule_assignment = frappe.get_doc(
@@ -25,5 +24,5 @@ def execute():
 			}
 		).insert()
 
-		for d in frappe.get_all("Shift Assignment", filters={"schedule": doc_name}, pluck="name"):
+		for d in frappe.get_all("Shift Assignment", filters={"schedule": doc.name}, pluck="name"):
 			frappe.db.set_value("Shift Assignment", d, "shift_schedule_assignment", schedule_assignment.name)
