7f0bf96640f85800c533e389dbb62783b0876af8
Asmita Hase <asmitahase@gmail.com>
2025-01-16 11:44:51 +0530

    chore: dropped "HR" from "HR Overview"

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -7,14 +7,14 @@ app_license = "GNU General Public License (v3)"
 required_apps = ["frappe/erpnext"]
 source_link = "http://github.com/frappe/hrms"
 app_logo_url = "/assets/hrms/images/frappe-hr-logo.svg"
-app_home = "/app/hr-overview"
+app_home = "/app/overview"
 
 add_to_apps_screen = [
 	{
 		"name": "hrms",
 		"logo": "/assets/hrms/images/frappe-hr-logo.svg",
 		"title": "Frappe HR",
-		"route": "/app/hr-overview",
+		"route": "/app/overview",
 		"has_permission": "hrms.hr.utils.check_app_permission",
 	}
 ]
