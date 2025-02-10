6917c6c4245ddff7e7c1e2b0004621162d325315
Asmita Hase <asmitahase@gmail.com>
2025-02-10 11:33:38 +0530

    refactor: new ref for loading state isn't required

--- frontend/src/components/CheckInPanel.vue
+++ frontend/src/components/CheckInPanel.vue
@@ -69,7 +69,7 @@
 				</div>
 			</template>
 
-			<Button :loading="checkingIn" variant="solid" class="w-full py-5 text-sm disabled:bg-gray-700" @click="submitLog(nextAction.action)">
+			<Button :loading="checkins.insert.loading" variant="solid" class="w-full py-5 text-sm disabled:bg-gray-700" @click="submitLog(nextAction.action)">
 				{{ __("Confirm {0}", [nextAction.label]) }}
 			</Button>
 		</div>
@@ -93,7 +93,6 @@ const checkinTimestamp = ref(null)
 const latitude = ref(0)
 const longitude = ref(0)
 const locationStatus = ref("")
-const checkingIn = ref(false)
 const settings = createResource({
 	url: "hrms.api.get_hr_settings",
 	auto: true,
@@ -158,7 +157,7 @@ const handleEmployeeCheckin = () => {
 
 const submitLog = (logType) => {
 	const actionLabel = logType === "IN" ? __("Check-in") : __("Check-out")
-	checkingIn.value = true
+
 	checkins.insert.submit(
 		{
 			employee: employee.data.name,
@@ -169,7 +168,6 @@ const submitLog = (logType) => {
 		},
 		{
 			onSuccess() {
-				checkingIn.value = false
 				modalController.dismiss()
 				toast({
 					title: __("Success"),
@@ -180,7 +178,6 @@ const submitLog = (logType) => {
 				})
 			},
 			onError() {
-				checkingIn.value = false
 				toast({
 					title: __("Error"),
 					text: __("{0} failed!", [actionLabel]),
