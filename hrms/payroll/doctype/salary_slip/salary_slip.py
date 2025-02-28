7549d49967d6ea01cc663a19a07c01a4d3dec2e7
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-02-28 18:50:23 +0530

    fix: reset loans only if field exists

--- hrms/payroll/doctype/salary_slip/salary_slip.py
+++ hrms/payroll/doctype/salary_slip/salary_slip.py
@@ -324,9 +324,10 @@ class SalarySlip(TransactionBase):
 	def get_emp_and_working_day_details(self):
 		"""First time, load all the components from salary structure"""
 		if self.employee:
-			self.set("loans", [])
 			self.set("earnings", [])
 			self.set("deductions", [])
+			if hasattr(self, "loans"):
+				self.set("loans", [])
 
 			if self.payroll_frequency:
 				self.get_date_details()
