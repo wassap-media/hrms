feb54d2d020e0d364ff87a007a6704fc987205fb
Gavin D'souza <gavin18d@gmail.com>
2025-03-18 11:53:14 +0100

    Revert "fix(Leave Application): Check presence of any submitted attendance"
    This reverts commit 80a2d5b030893924ddc9f9fc9d607a330ea4691b.

--- hrms/hr/doctype/leave_application/leave_application.py
+++ hrms/hr/doctype/leave_application/leave_application.py
@@ -554,6 +554,7 @@ class LeaveApplication(Document, PWANotificationsMixin):
 			filters={
 				"employee": self.employee,
 				"attendance_date": ("between", [self.from_date, self.to_date]),
+				"status": ("in", ["Present", "Half Day", "Work From Home"]),
 				"docstatus": 1,
 			},
 			fields=["name", "attendance_date"],
